a5bd992647456828c5c20e28666f9466d192f72e
Armougom <y.armougom@gmail.com>
2019-10-08 22:19:37 +0200

    ajout animation logo react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-
-function App() {
+import QuoteList from './components/QuoteList';
+import './App.css'
+import Lamp from './Lamp'
+import Logo from './logo512.png';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        working: props.working
+      };
+  }
+  changeWorking = () =>{
+    this.setState({ working: !this.state.working });
+  }
+  
+  render(){
+    const light = this.state.working ? "App-logo" : "secoue";
   return (
     <div className="App">
       <Navbar />
+      <Lamp />
+      <Lamp />
+      <div className="center">
+      <button onClick={this.changeWorking}>Bouge !!</button>
+        <img className={light}  src={Logo} alt="Logo"/>
+      </div>
+      
+      <QuoteList />
     </div>
   );
+  }
 }
 
 export default App;
